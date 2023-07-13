14f1076dcf79a67becef7c61d1438a9b34a6f78d
jmosqueradeng <mathiaselgordo@gmail.com>
2023-07-12 20:51:22 -0500

    app-hola-mundo

---
+++ js/hola-mundo.js
@@ -0,0 +1,7 @@
+// primera app en javascript
+/**
+ * app hola mundo utiliza una función llamada console.log();
+ */
+
+// muestra el resultado en la consola del navegador
+console.log('hola mundo');
