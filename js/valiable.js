57548ad0d982598c0696eef8c47a82e1a10f31d8
jmosqueradeng <mathiaselgordo@gmail.com>
2023-07-12 21:16:19 -0500

    app-variables

---
+++ js/valiable.js
@@ -0,0 +1,18 @@
+/**
+ * para declarar la variable var, lt y const
+ * tipos datos:null, undefined, string(texto), boolean(true,false), number, symbol y object
+ */
+
+let miEdad=28; //tipo numero
+
+const miNombre = 'juan-carlos'; // tipo texto
+
+const casado = 'falce'; // tipo booleano
+
+// miNombre = 'Fausto';
+
+console.log('mi nombre: ' + miNombre);
+console.log('mi edad: ' + miEdad);
+console.log('Estado Civil: ' + casado);
+
+
